207822c95cd65cc2a03d3639f7b7e931745b9f46
Moises Croes <mcg-360@hotmail.com>
2022-05-03 02:38:08 +0200

    Functions and methods

--- oop/class.go
+++ oop/class.go
@@ -7,10 +7,31 @@ type Employee struct {
 	name string
 }
 
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
 	fmt.Printf("%v", e)
 	e.id = 1
 	e.name = "Name"
-	fmt.Printf("%+v", e)
+	fmt.Println(e)
+	e.SetId(5)
+	e.SetName("Name2")
+	fmt.Println(e)
+	fmt.Println(e.GetName())
+	fmt.Println(e.GetId())
 }
